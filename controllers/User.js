76643cc701396e2d914c7c60d2f47e9ec029be10
Tom Tamen <tomtamen@gmx.fr>
2023-01-29 18:15:13 +0100

    :boom: mongoDB is on fire

--- controllers/User.js
+++ controllers/User.js
@@ -1,22 +1,33 @@
 const { User } = require("../models/User")
-const connect = require("../DBconnect")
 
-const UserController = class{
-    
-    constructor(req){
-        this.name = req.body.pseudo
-        this.mail = req.body.mail
-        this.password = req.body.password
-    }
 
-    async addUser(){
-        try{
-            await new User({name: this.name, mail :this.mail, password:this.password, isAdmin: false}).save()
-        }catch(error){
-            return [400, error]
-        }
-        return [200, "FAIT"]
+
+const addUser = async (req) => {
+    try {
+        if(req.body.password!=req.body.confPassword) throw new Error('Mots de passes différents')
+        await new User({ name: req.body.pseudo, mail: req.body.mail, password: req.body.password, isAdmin: false }).save()
+    } catch (error) {
+        return [400, error]
     }
+    return [200, "FAIT"]
 }
 
-module.exports = { UserController };
+const getAllUsers = async () => {
+    
+    return User.find({}
+        , {
+            "name": 1,
+            "mail": 1
+        }
+    )
+}
+
+// const login = async ()=>{
+
+// }
+
+module.exports = {
+    addUser,
+    getAllUsers,
+    //login
+}
