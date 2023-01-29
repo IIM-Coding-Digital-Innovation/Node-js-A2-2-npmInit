2b8be9d9e5f26d0bc24fb8dab90e9523b9587cce
Tom Tamen <tomtamen@gmx.fr>
2023-01-29 23:42:13 +0100

    :constrution: middleware isadmin

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 
 module.exports = (req,res,next)=>{
     try{
-        if(jwt.verify(req.params.token, process.env.SECRET_KEY).isAdmin){
+        let token
+        if(req.params.token){
+            token = req.params.token
+        }else{
+            token = req.headers.authorization.split(' ')[1];
+        }
+        if(jwt.verify(token, process.env.SECRET_KEY).isAdmin){
             next()
         }else{
             res.redirect('/login')
